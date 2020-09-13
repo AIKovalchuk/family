e7711daa0d5f285ee4fae2605b894c3364098a46
AleksandrK <akovalchuk@tribit.ru>
2020-09-13 13:16:47 +0300

    add logo

--- client/components/layout/header/Logo.jsx
+++ client/components/layout/header/Logo.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Logo = () => {
     return (
         <div className="logo">
-            Logo
+            <img
+                src="http://guides.gosuslugi.ru/download.html?file=/765/5.svg&amp;dl=0"
+                alt=""
+                width="48"
+                height="48"
+            />
         </div>
     )
 }
